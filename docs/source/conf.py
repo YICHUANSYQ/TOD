8736c19b8c19f2da33dfb6a255364ab619f06228
YICHUANSYQ <1119150971@qq.com>
2024-01-25 14:33:52 +0800

    make html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# extensions = []
 extensions = ['recommonmark','sphinx_markdown_tables'] 
 
 templates_path = ['_templates']
@@ -27,5 +26,4 @@ language = 'zh_CN'
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# html_theme = 'alabaster'
-# html_static_path = ['_static']
+html_static_path = ['_static']
